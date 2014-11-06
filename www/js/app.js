b4b9faadd14ad0490dbb7cefbb8dc2a098cc4a17
Khashayar Pourdeilami <kpourdeilami@gmail.com>
2014-11-05 20:53:48 -0500

    initial commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,25 @@
-// Ionic Starter App
+/*
+App Initialization
+ */
+var app = angular.module('humansVsZombies', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('humansVsZombies', ['ionic'])
 
-.run(function($ionicPlatform) {
+
+/*
+ Services
+ */
+
+/*
+ Routing
+ */
+
+
+
+/*
+ Controllers
+ */
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +30,8 @@ angular.module('humansVsZombies', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+/*
+ Helper functions
+ */
